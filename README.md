29fbe0a35589e0be684577e97ffa2837cabc9736
root <you@example.com>
2017-05-18 14:31:35 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# advancedrequest
+
+> ES6 JS class for intervaled requests with retries and timeouts
+
+## Installation
+
+```sh
+npm install advancedrequest --save
+```
+
+## Usage
+
+Manager for sending HTTP requests with multiple tries, wait intervals, and timeouts  
+Uses node module 'request'  
+
+This can be configured by adding names as keys and values with requiredInterval  
+in order to WAIT before sending another request of that name. If you wanted to  
+only send one request with name DevAPI_SendFriendRequest per minute, set the  
+requiredInterval to 1000*60 and the minute will automatically be enforced  
+
+All 'lastruns' start with the moment of script start  
+to make sure script restarts don't accidentally disobey the interval  
+
+
+## Simple GET request example
+```js
+var advancedrequest = require('advancedrequest');
+
+new advancedrequest.AdvancedRequest({
+    url: "http://api.somefriendsite.com/addFriend",
+    callback: function (dataString) {
+      console.log("[+] API response received!", dataString);
+    }
+}).run();
+```
+
+## Inheritance example
+
+Here you can define our own logic to determine if a request was successful  
+or needs to be retried by subclassing advancedrequest.AdvancedRequest
+```js
+class DevApiRequest extends advancedrequest.AdvancedRequest {
+  constructor (args) {
+    super(args); // always call superclass constructor!
+  }
+  // Override the postProcess function to provide your own checks
+  postProcess () {
+    // this.data is the body of the request.
+    if (!this.data) {
+      // If the response was blank, call this.fail to retry the same request in 10 seconds
+      this.fail(10, "Request was blank! Is connection to internet broken?");
+    } else if (this.data.indexOf('Too many requests in the last hour!') != -1) {
+      // If the API has a limit and tells you about it, you can call this.fail and retry in 1 hour
+      this.fail(60*60, "Hit rate limit. Waiting 1 hour to retry");
+    } else {
+      // All was clear! MAKE SURE YOU HAVE this line at the end of your request!
+      this.onFinish(this.data);
+    }
+  }
+};
+
+
+// Then once you've written your DevApiRequest class, maybe you'd call it so:
+function sendFriendRequestTo(targetId, callback) {
+
+  var sendFriendRequestReq = new DevApiRequest({
+      url: "http://api.somefriendsite.com/addFriend",
+      method: "POST",
+      name: "sendFriendRequest", // NOTE: used as identifier for interval
+      postData: { targetId: targetId, auth_token: "24t4534token42i5h2"},
+      callback: callback
+  });
+  sendFriendRequestReq.run();
+
+}
+sendFriendRequestTo(252452, function(json) {
+  console.log("[+] API response received!", json);
+});
+```
+
+## Intervaled request example
+
+Enforce a defined interval between requests with the same 'name' passed into them.
+Using the DevApiRequest class, we first set lastRunHash with the proper interval
+```js
+// Set interval timeouts for the requests of a particular name
+advancedrequest.setLastRunHash({
+  'sendFriendRequest': { requiredInterval: 1000*15 }, // 15 seconds
+  'removeFriend':      { requiredInterval: 1000*5, lastReqTime: new Date() }
+});
+
+sendFriendRequestTo(34, function(json) {
+  // 15 seconds will be waited before next request is sent out
+  sendFriendRequestTo(35, function(json) {
+    // 15 seconds will be waited before next request is sent out
+    sendFriendRequestTo(36, function(json) {
+      console.log("[+] 3 friend requests sent successfully with 15 seconds between them");
+    });
+  });
+});
+```
+
+## Credits
+http://c-cfalcon.rhcloud.com
---
+++ index.js
@@ -0,0 +1,229 @@
+
+var request = require('request'); // https://github.com/request/request
+
+/**
+ * @class AdvancedRequest
+ *
+ * Manager for sending HTTP requests with multiple tries, wait intervals, and timeouts
+ * Uses node module 'request'
+ *
+ * pass in format of:
+ * {
+ *   'url': url,
+ *   'method': "POST", // or "GET"
+ *   'callback': callback,
+ *   'postData': {asdfa: "asdf", "okef": "efe" }
+ * }
+*/
+
+
+/**
+ * lastRunHash
+ *
+ * This can be configured by adding names as keys and values with requiredInterval
+ * in order to WAIT before sending another request of that name. If you wanted to
+ * only send one request with name DevAPI_SendFriendRequest per minute, set the
+ * requiredInterval to 1000*60 and the minute will automatically be enforced
+ *
+ * All 'lastruns' start with the moment of script start
+ * to make sure script restarts don't accidentally disobey the interval
+ *
+ */
+var lastRunHash = {
+    //'WebAPIfollowCMD': { lastReqTime: new Date(), requiredInterval: 1000*10 }, // 10 seconds
+    //'WebAPIunfollowCMD': { lastReqTime: new Date(), requiredInterval: 1000*60*10 }, // 10 minutes
+};
+
+class AdvancedRequest {
+  constructor (args) {
+    this.url = args.url;
+    this.method = args.method || "GET";
+    this.postData = args.postData;
+
+    // stringify if necessary
+    this.postData = (typeof(this.postData) == "object") ? JSON.stringify(this.postData) : this.postData;
+
+    this.name = args.name || "unnamed request";
+    this.maxRetries = args.maxRetries || 10; // Pass in 0 for unlimited
+
+    // Pass this in as true to avoid multipart header. This is rare; some API endpoints need it
+    this.noMultipartHeader = args.noMultipartHeader || false;
+
+    // If downloading file to disk, pass in this as the raw filename to save as
+    this.saveAs = args.saveAs;
+    this.isBinaryRequest = args.isBinaryRequest || this.saveAs;
+
+    this.callback = args.callback || function () {
+        console.log("[!] No request callback provided!");
+    };
+
+    this.numTriesSoFar = 0; // start at zero
+    this.data = ''; // Request data
+
+    this.apiObj = args.apiObj || {};
+
+    this.requestHeaders = {}; // nodejs request sent OUT
+    this.responseHeaders = [];  // headers for the request RESPONSE
+  }
+
+  /**
+   * Provide the FULL header. ex:
+   * addHeader('Cookie: awefaf=ewfef;wefweafwef');
+   */
+  addHeader (fullHeader) {
+    var pieces = fullHeader.split(': ');
+    this.requestHeaders[pieces[0]] = pieces[1]; // Could do .toLowerCase() on pieces[0]
+  }
+
+  getResponseHeaders () {
+    return this.responseHeaders;
+  }
+
+  /**
+   * requestRetriesExhausted
+   *
+   * Occurs when the request has called .fail enough times to equal the 'this.maxRetries' count
+   * You may want to override this function as your needs demand
+   */
+  requestRetriesExhausted () {
+    console.log("[!] Max request retries exceeded for request named (", this.name, ")", ". Quitting process!");
+    require('process').exit(0);
+  }
+
+  /**
+   * fail
+   *
+   * Increment retries counter and retry the request in 'sleepSeconds' seconds
+   * or KILL the node process if too many retries have happened
+   */
+  fail (sleepSeconds, additionalMsg) {
+    this.numTriesSoFar++;
+
+    console.log("[!]", additionalMsg || 'Req fail!',
+      "Status:", this.responseStatusCode, "url:", this.url, "name:", this.name,
+      "tries left:", this.maxRetries - this.numTriesSoFar, "options:", this.options);
+
+    if (this.numTriesSoFar >= this.maxRetries && this.maxRetries != 0) {
+        return this.onRequestRetriesExhausted();
+    }
+
+    setTimeout(this.run.bind(this), sleepSeconds*1000.0); // convert to full seconds here
+  }
+
+  onFinish (result) {
+    // Update last run time for this request if applicable. (in future add username?)
+    if (lastRunHash[this.name]) {
+      // We use THIS moment, the END of the request as the marker.
+      lastRunHash[this.name].lastReqTime = new Date();
+    }
+
+    return this.callback(result);
+  }
+
+  /**
+   * postProcess
+   *
+   * Recommended that this be overridden to check for particular error types
+   */
+  postProcess () {
+    console.log("[D] NOTE: AdvancedRequest.postProcess probably should be overridden " +
+      "(usually through inheritance) and given verification criteria");
+
+    // for example, override and have a check:
+    // if (!this.data) return this.fail(10, "Response was blank! Retrying in 10 seconds");
+
+    // In your subclass, remember to include this line to complete the request
+    this.onFinish(this.data);
+  }
+
+  isSleepIntervalNecessary () {
+    // If this is a request with limitations
+    if (lastRunHash[this.name]) {
+      // initialize now if not initialized yet
+      if (!lastRunHash[this.name].lastReqTime) {
+        lastRunHash[this.name].lastReqTime = new Date();
+      }
+
+      var millisecondsSinceLastRequest = (new Date() - lastRunHash[this.name].lastReqTime);
+      return millisecondsSinceLastRequest < lastRunHash[this.name].requiredInterval;
+    }
+    return false;
+  }
+
+  sleepIntervalIfNecessary (callback) {
+    if (this.isSleepIntervalNecessary()) {
+      var millisecondsSinceLastRequest = (new Date() - lastRunHash[this.name].lastReqTime);
+      var timeToSleep = lastRunHash[this.name].requiredInterval - millisecondsSinceLastRequest;
+      console.log("[D] Sleeping", timeToSleep/(1000), "seconds now");
+
+      setTimeout(() => {this.sleepIntervalIfNecessary(callback);}, timeToSleep);
+    } else {
+      return callback();
+    }
+  }
+
+  /**
+   * Perform request
+   */
+  run () {
+    if (this.isSleepIntervalNecessary()) {
+      return this.sleepIntervalIfNecessary(() => {this.run.apply(this, arguments); });
+    }
+
+    this.options = {
+      url: this.url,
+      method: this.method,
+      headers: this.requestHeaders,
+      gzip: true,
+      timeout: 60*1000, // number of ms to wait for response headers (1 min)
+
+      // https://github.com/nodejs/node/issues/3692 UGGHHHH this bug results in
+      //  a call to this.fail(10) then next request works
+      //agentOptions: { ciphers: 'ALL', secureProtocol: 'TLSv1_method', },
+    };
+
+    // If we're saving, we want an image file and therefore want the BUFFER
+    // received, instead of the default "string". To maybe sha1 it, etc
+    if (this.isBinaryRequest) {
+      this.options["encoding"] = null; // set to null to get binary and not string
+    }
+    if (this.postData) {
+      if (this.noMultipartHeader) {
+        this.options["form"] = this.postData;
+      } else {
+        this.options["multipart"] = [ {
+          //'Content-Type': 'application/x-www-form-urlencoded',
+          body: (typeof(this.postData) == "string") ? this.postData : JSON.stringify(this.postData),
+        } ];
+      }
+    }
+
+    var r = request(this.options, (error, response, body) => {
+      if (error) {
+        // this.fail helps for bugs WITH NO KNOWN FIX LIKE: routines:SSL3_GET_RECORD:wrong version number:
+        return this.fail(10, "ERROR with advanced request code somehow!! Err:" + error);
+      } else {
+        // body will be Buffer if isBinaryRequest or string otherwise
+        this.data = body;
+        this.responseStatusCode = (response) ? response.statusCode : -1;
+        this.responseHeaders = (response) ? response.headers : [];
+        // This will call the subclassed 'postProcess' if this class has been extended
+        return this.postProcess();
+      }
+    });
+
+    // Quickly attach pipe call before tick ends. Next tick will be request call!
+    if (this.saveAs) {
+      requ.pipe(fs.createWriteStream(this.saveAs));
+    }
+  }
+};
+
+
+
+module.exports = {
+  AdvancedRequest: AdvancedRequest,
+  setLastRunHash: function (newLastRunHash) {
+    lastRunHash = newLastRunHash;
+  }
+};
